7f22b06d4a8b7b2b06c64615df06418a8f660221
fs203 <fs2031@icloud.com>
2024-09-14 20:20:56 +0900

    update

--- sample.c
+++ sample.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-    // int apple;
-    // int orange;
-    // apple = 5;
-    // orange = 10;
-
-    int apple = 10;
-    int orange = 5;
-    printf("%d", apple + orange);
+    int apple;
+    apple = 10;
+    apple = apple + 5;
+    printf("%d", apple);
+    return 0;
 }
