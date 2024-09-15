d843534248a55b0176878cf80a941420b793fe19
fs203 <fs2031@icloud.com>
2024-09-16 08:35:06 +0900

    update

--- sample.c
+++ sample.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    int apple = 10;
-    printf("%d\n", apple);
-
-    apple ++;
-    printf("%d\n", apple);
-
-    apple ++;
-    printf("%d\n", apple);
-
-    apple --;
-    printf("%d\n", apple);
+    printf("%d", (int) (3.14*3*3));
     return 0;
 }
-
 // 09-14 fnished this video : 1:31:01/4:38:24
