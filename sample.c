0a9b3ef4f36c1979c8b813450a5093db373b1a68
fs203 <fs2031@icloud.com>
2024-09-17 18:49:37 +0900

    update

--- sample.c
+++ sample.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-    int apple = 10;
-    if (apple-10) printf("リンゴの数は0個ではありません");
+    int number;
+    scanf("%d", &number);
+    if (number < 10) printf("10未満です");
+    if (number > 10) printf("10より大きいです");
+    if (number == 10) printf("10と等しいです");
     return 0;
 }
+// 1:47:58/4:38:24
