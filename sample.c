0cfd0a0022d0959d492ab1e40f6e211ff177eb80
fs203 <fs2031@icloud.com>
2024-09-17 18:53:20 +0900

    update

--- sample.c
+++ sample.c
@@ -3,9 +3,7 @@ int main(void)
 {
     int number;
     scanf("%d", &number);
-    if (number < 10) printf("10未満です");
-    if (number > 10) printf("10より大きいです");
-    if (number == 10) printf("10と等しいです");
+    if (number >= 5 && number <= 10) printf("5~10の間です");
+    if (number < 5 || number > 10) printf("5~10の間ではありません");
     return 0;
 }
-// 1:47:58/4:38:24
