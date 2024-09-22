354d0d877be671bac9df84bc10d6404b483fb33b
fs203 <fs2031@icloud.com>
2024-09-22 19:17:42 +0900

    update

--- sample.c
+++ sample.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-    int number;
-    scanf("%d", &number);
-    if (number == 10) {
-        printf("10とおなじです\n");
-        printf("正しく入力できました\n");
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf ("%d回目：ありがとう\n", i);
     }
-
+    return 0;
 }
+// 2:01:43/
