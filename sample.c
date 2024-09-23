8937997804f59b625b539c93b9e17da5ea91b472
fs203 <fs2031@icloud.com>
2024-09-24 08:48:34 +0900

    update

--- sample.c
+++ sample.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-    int i;
-    for (i = 1; i <= 10; i++) {
-        printf ("%d回目：ありがとう\n", i);
+    int year = 1;
+    double money = 1000000;
+    while (money < 100000000) {
+        year++;
+        money = (money + 600000) * 1.05;
     }
+    
+    printf("%d年目：%f 円となり、資産が１億円を超えます", year, money);
     return 0;
 }
-// 2:01:43/
+// 2:023:46/
