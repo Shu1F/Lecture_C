ac97b2e4f36a878bbf7c218fcdf91da7f8131ab0
fs203 <fs2031@icloud.com>
2024-10-01 00:34:44 +0900

    update

--- sample.c
+++ sample.c
@@ -1,24 +1,8 @@
 #include <stdio.h>
-int fruits(int apple);
+int apple = 10;
 int main(void)
 {
-    int hoge;
-    hoge = fruits(5);
-    if (hoge < 3000)
-    {
-        printf("予算に収まりました");
-    }
-    else
-    {
-        ("予算オーバーです");
-    }
-    printf("%d", hoge);
+    int apple = 5;
+    printf("%d", apple);
     return 0;
-}
-
-int fruits(int apple)
-{
-    int sum;
-    sum = apple * 300;
-    return sum;
 }
