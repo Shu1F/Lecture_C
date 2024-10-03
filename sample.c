8152823d92074f2112d539c9fa0b85f0cac39478
fs203 <fs2031@icloud.com>
2024-10-03 11:54:05 +0900

    update

--- sample.c
+++ sample.c
@@ -1,10 +1,22 @@
+// #include <stdio.h>
+// int main(void)
+// {
+//     int array [5] = {12, 13, 14};
+//     int i;
+//     for (i = 0; i < 5; i++) {
+//         printf("%d番目：%d\n", i, array[i]);
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
+#include <string.h>
 int main(void)
 {
-    int array [5] = {12, 13, 14};
+    char str[256];
+    scanf("%s", str);
     int i;
-    for (i = 0; i < 5; i++) {
-        printf("%d番目：%d\n", i, array[i]);
-    }
+    i = strlen(str);
+    printf("%d", i);
     return 0;
 }
