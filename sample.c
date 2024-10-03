b41e2ba9b2ad7011b5b547aa13a517e18aa171bb
fs203 <fs2031@icloud.com>
2024-10-03 11:10:40 +0900

    update

--- sample.c
+++ sample.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-    int array[10];
-    array[5] = 15;
-    printf("%d\n", array[5]);
+    int array [5] = {12, 13, 14};
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("%d番目：%d\n", i, array[i]);
+    }
     return 0;
 }
